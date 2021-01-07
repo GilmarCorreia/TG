c4b7339b651e7776be57eba753f121dd1afbf2f9
GilmarCorreia <gilmarcorreiajeronimo@gmail.com>
2021-01-07 14:16:55 -0300

    add path to cover

--- Arm.py
+++ Arm.py
@@ -17,6 +17,13 @@ class Arm():
     _L3 = 10.650 #centimeters
     _a1 = 1.320  #centimeters
 
+    maxT3 = 866
+    minT3 = 160
+    maxT2 = 823
+    minT2 = 154 
+    maxT1 = 1023
+    minT1 = 0
+
     pHome = [[None, None, None],
              [None,None,None]]
     
@@ -25,6 +32,11 @@ class Arm():
         self.setHome()
         
     def FK(self,theta1, theta2, theta3):
+
+    	angles = [theta1,theta2,theta3]
+
+    	[theta1,theta2,theta3] = self.verifyAngles(angles)
+
         T1 = math.radians(theta1)
         T2 = math.radians(theta2)
         T3 = math.radians(theta3)
@@ -59,21 +71,69 @@ class Arm():
         
         angles = [math.degress(T1), math.degress(T2), math.degress(T3)]
         
-        anglesToDec = self.anglesToDec(angles)
+        anglesToDec = self.verifyDec(self.anglesToDec(angles))
         
         return anglesToDec
 
+    def verifyAngles(self, angles):
+    
+    	if angles[0] > self.maxT1:
+    		angles[0] = self.maxT1
+    	elif angles[0] < self.minT1:
+    		angles[0] = self.minT1 
+
+    	if angles[1] > self.maxT2:
+    		angles[1] = self.maxT2
+    	elif angles[1] < self.minT2:
+    		angles[1] = self.minT2
+
+    	if angles[2] > self.maxT3:
+    		angles[2] = self.maxT3
+    	elif angles[2] < self.minT3:
+    		angles[2] = self.minT3
+
+    	return angles
+
+    def verifyDec(self, decs):
+
+    	if decs[0] > self.maxT1:
+    		decs[0] = self.maxT1
+    	elif decs[0] < self.minT1:
+    		decs[0] = self.minT1 
+
+    	if decs[1] > self.maxT2:
+    		decs[1] = self.maxT2
+    	elif decs[1] < self.minT2:
+    		decs[1] = self.minT2
+
+    	if decs[2] > self.maxT3:
+    		decs[2] = self.maxT3
+    	elif decs[2] < self.minT3:
+    		decs[2] = self.minT3
+
+    	return decs
+
     def getServos(self):
         return self._servos
+
+    def maxAngles(self):
+    	maxAngles = self.decToAngles(self.maxT1, self.maxT2, self.maxT3)
+
+    	return maxAngles
+
+    def minAngles(self):
+    	minAngles = self.decToAngles(self.minT1, self.minT2, self.minT3)
+
+    	return minAngles
     
     def anglesToDec(self, angles):
         anglesToDec = [1023.0-(512.0+((511.0/150.0)*(90.0+angles[0]))), 1023.0-(512.0+((angles[1]-90.0)*(511.0/150.0))), 1023.0-(512.0+(511.0/150.0)*angles[2])]
-        
+
         return anglesToDec
     
     def decToAngles(self, dec):
         decToAngles = [((150.0/511.0)*(511.0-dec[0]))-90.0, ((150.0/511.0)*(511.0-dec[1]))+90.0, (150.0/511.0)*(511.0-dec[2])]
-        
+
         return decToAngles
 
     def setHome(self):
@@ -92,4 +152,36 @@ class Arm():
         self.pHome[1] = [tac.getDec()[0],tac.getDec()[1],tac.getDec()[2]]
         
         print("pHome = ({:.2f}, {:.2f}, {:.2f})\n".format(self.pHome[0][0], self.pHome[0][1], self.pHome[0][2]))
+
+        self.runMapping()
+
+    def coveredPath(self):
+
+    	x_p = np.arrange(0,10,0.1).toList()
+    	y_p = np.arrange(-5,5,0.1).toList()
+    	z_p = [0] * x_p.length()
+
+    	path = [[x,y,0] for x in x_p for y in y_p]
+
+   		return path
+
+   	def verifyPoint(self, point):
+   		angles = self.IK(point[0],point[1],point[2])
+   		posArm = self.FK(angles[0],angles[1],angles[2])
+
+   		dist = math.sqrt(math.pow(point[0] - posArm[0],2) + math.pow(point[1] - posArm[1],2) + math.pow(point[2] - posArm[2],2))
+
+   		if dist <= 0.5:
+   			return True
+   		else:
+   			return False
+
+   	def runMapping(self):
+
+   		path = self.coveredPath()
+
+   		for point in path:
+   			if self.verifyPoint(point):
+   				self.arm.move(self.IK(point[0],point[1],point[2]))
+   				time.sleep(0.5)
 
--- Run.py
+++ Run.py
@@ -1,7 +1,11 @@
 ## -*- coding: utf-8 -*-
 #from TouchSensorArduino import TouchSensorArduino
 from Arm import Arm
+import matplotlib.pyplot as plt
+import numpy as np
+import math
 
 #ts = TouchSensorArduino()
+
 
 robo = Arm()
